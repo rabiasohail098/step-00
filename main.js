5243293adae6995470a306b76d72a51cefb0aa2c
rabiasohail098 <rabiasohail1209@gmail.com>
2024-07-12 07:25:20 +0500

    step 00

---
+++ main.js
@@ -0,0 +1,22 @@
+"use strict";
+// step00_helloworld
+//step00a_json_objects
+let object = {
+    "name": "Rabia",
+    "age": 35,
+    "enroll": true,
+    "address": {
+        "city": "Karachi",
+        "country": "Pakistan"
+    },
+    "hobbies": [
+        "Reading",
+        "Coding",
+        "Outing",
+        "Cooking"
+    ]
+};
+console.log(object.address);
+//step00b_syntax_error
+//step00c_type_error
+//step00d_assignability_error
---
+++ main.ts
@@ -0,0 +1,38 @@
+// step00_helloworld
+//step00a_json_objects
+
+let object = {
+    "name":"Rabia",
+    "age":35,
+    "enroll":true,
+    "address":{
+        "city":"Karachi",
+        "country":"Pakistan"
+    },
+    "hobbies":[
+        "Reading",
+        "Coding",
+        "Outing",
+        "Cooking"
+    ]
+}
+
+console.log(object.address);
+
+
+//step00b_syntax_error
+// spelling mistake
+lett myName = "Rabia";
+
+// punctuation error
+let age = 35:
+
+// brackets mistake
+// statement mistake
+
+//step00c_type_error represent data type
+let color:string = 35
+
+//step00d_assignability_error
+let car = "Toyota";
+car = 46
